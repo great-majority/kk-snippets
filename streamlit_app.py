57c904d1c6bfd1e6d91d3fafa2a49db360fa1bc5
多数 <40860726+great-majority@users.noreply.github.com>
2024-09-08 06:13:22 +0000

    トップページをちょっと変更

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,20 @@
 
 import streamlit as st
 
-title = "トップページ"
+title = "kk-snippets"
 st.set_page_config(page_title=title)
 st.title(title)
 
 text = """
-# このページは?
-コイカツ/ハニカムに関連するちょっとしたツールを置くページです。
+コイカツ/ハニカム/サマすくに関連するツールを置くページです。
 
 動作の不具合などあれば [@tropical_362827](https://twitter.com/tropical_362827) に送っていただけると助かります :bow:
 
 ←のメニューからページを選択してください。
+
+<a href="https://github.com/great-majority/kk-snippets">
+    <img src="https://opengraph.githubassets.com/24d9aa2fc57ba5cc9c57898c8c43138d09b4ed7e71ede99bbe892ec0b7de6ded/great-majority/kk-snippets" alt="githubリンク" width="80%">
+</a>
 """
 
-st.markdown(text)
+st.markdown(text, unsafe_allow_html=True)
